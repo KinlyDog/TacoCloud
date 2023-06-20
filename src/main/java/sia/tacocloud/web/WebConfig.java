a437e151a2b7e00335138f574cad8174c8bafaa5
KinlyDog <kinlydog@gmail.com>
2023-06-20 17:36:29 +1000

    Update project

---
+++ src/main/java/sia/tacocloud/web/WebConfig.java
@@ -0,0 +1,17 @@
+package sia.tacocloud.web;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/*
+Аналог класса HomeController для создания представления "/"
+Если удалить HC, то надо также удалить его из ссылки на тестирование.
+ */
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("home");
+    }
+}
